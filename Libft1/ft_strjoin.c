644f6009fbb064cb903f96f8337b7f35ef0297f6
root <root@LAPTOP-25FHSK79>
2023-09-27 13:06:01 +0200

    intentando completar el strjoin

--- Libft1/ft_strjoin.c
+++ Libft1/ft_strjoin.c
@@ -6,18 +6,24 @@
 /*   By: gmarin-m <gmarin-m@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 11:04:51 by gmarin-m          #+#    #+#             */
-/*   Updated: 2023/09/26 09:01:26 by gmarin-m         ###   ########.fr       */
+/*   Updated: 2023/09/27 12:41:39 by gmarin-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// s1: la primera string 
-// s2: la string a añadir a s1.
+#include "libft.h"
 
-// valor devuelto la nueva string.
-// null si falla la reserva de memoria
+// se utiliza malloc. 
 
+// la primera string.    //s3 es la string a añadir a s1
 char *ft_strjoin (char const *s1, char const *s2)
 {
+    char *s3 = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 
+    // devuelve null si falla la reserva de memoria.
 
+    // reservamos con malloc y devolvemos una nueva string que resulta de
+    // la concatenacion de 's1' y 's2'.
+
+    
+return s3;
 }
