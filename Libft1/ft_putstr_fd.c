a6a27bfeb2251e8c5e11488eadffd27d2df8a3f4
root <root@LAPTOP-25FHSK79>
2023-10-10 12:53:36 +0200

    hecho hasta el putstr_fd

--- Libft1/ft_putstr_fd.c
+++ Libft1/ft_putstr_fd.c
@@ -9,4 +9,20 @@
 /*   Updated: 2023/09/25 20:21:10 by gmarin-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
+#include <stdio.h>
 
+// s: La string a enviar.
+// fd: El file descriptor sobre el que escribir.
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		write (fd, &s[i], 1);
+		i ++;
+	}
+}
